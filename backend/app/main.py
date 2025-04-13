e39d346231b6b78446c92b93860c0092fc51db8a
Amit <yadavamitt31@gmail.com>
2025-04-13 22:39:00 +0530

    frontend url

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://video-downloader-three-umber.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
